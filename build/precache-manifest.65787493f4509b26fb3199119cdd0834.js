a180d2c7f67a6481f5648b7f569cd1e69e5d1554
sidkhuntia <siddharthakhuntia@gmail.com>
2021-12-05 16:06:07 +0530

    deployment_test

---
+++ build/precache-manifest.65787493f4509b26fb3199119cdd0834.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2459b74c9df94d911df9c1fec268850",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d478e7f699f8a4fe0e4f",
+    "url": "/static/js/2.fc726699.chunk.js"
+  },
+  {
+    "revision": "b41bfafb967cb65a63e7",
+    "url": "/static/js/main.d74c8517.chunk.js"
+  },
+  {
+    "revision": "f3ab9bf2fde6b43969db",
+    "url": "/static/js/runtime-main.636b7682.js"
+  }
+]);
